ffae5aefa628d85607777172826fea801211c6eb
gary <gary@MACSTATION.local>
2019-12-19 22:31:22 +0000

    Updates

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -176,7 +176,6 @@ export class App extends React.Component<any, AppState> {
         this._apiClient = new ApiClient(this._configuration.app.apiBaseUrl, this._authenticator);
 
         // Subscribe to windows events
-        window.onhashchange = this._onHashChange;
         window.onresize = this._onResize;
     }
 
