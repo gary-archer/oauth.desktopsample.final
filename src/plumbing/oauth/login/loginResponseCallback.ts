36c5effc38555a3144b747181921f8aae1952ab7
Gary Archer <Gary.Archer@collinsongroup.com>
2020-02-25 18:38:11 +0000

    Updates

--- src/plumbing/oauth/login/loginResponseCallback.ts
+++ src/plumbing/oauth/login/loginResponseCallback.ts
@@ -1,4 +1,2 @@
-import {UIError} from '../../errors/uiError';
-
 // A custom type for readability
 export type LoginResponseCallback = (queryParams: any) => void;
--- src/plumbing/oauth/logout/logoutResponseCallback.ts
+++ src/plumbing/oauth/logout/logoutResponseCallback.ts
@@ -1,4 +1,2 @@
-import {UIError} from '../../errors/uiError';
-
 // A custom type for readability
 export type LogoutResponseCallback = (queryParams: any) => void;
