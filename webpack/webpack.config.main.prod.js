0b82f83b33a087ce0775bd0b1603f3e093037a38
gary <gary@MACSTATION.local>
2020-07-05 21:47:25 +0100

    Updates

--- webpack/webpack.config.main.prod.js
+++ webpack/webpack.config.main.prod.js
@@ -1,5 +1,4 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const baseConfig = require('./webpack.config.main.base.js');
 
 module.exports = merge(baseConfig, {
