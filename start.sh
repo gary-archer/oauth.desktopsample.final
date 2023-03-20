866a6a3ca05c82101e580dad65452b5f981072c2
gary <garyarcher36@gmail.com>
2023-03-20 18:32:16 +0000

    Updates

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-##############################################
+##########################################################################
 # A script to build the desktop app and run it
-##############################################
+# On Windows, ensure that you have first set Git bash as the node.js shell
+# npm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"
+##########################################################################
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
