21a1d0014434530ca9d6e752c8a148bc65dbf45d
gary <gary.archer36@gmail.com>
2024-02-14 19:19:03 +0000

    Updates

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,9 @@ class Main {
      */
     public execute(): void {
 
+        console.log('*** DATA LOCATION ***');
+        console.log(app.getPath('userData'));
+
         // Prevent private URI scheme notifications on Windows + Linux from creating a new instance of the application
         const primaryInstance = app.requestSingleInstanceLock();
         if (!primaryInstance) {
--- src/plumbing/oauth/tokenStorage.ts
+++ src/plumbing/oauth/tokenStorage.ts
@@ -4,18 +4,25 @@ import {TokenData} from './tokenData';
 
 /*
  * A class to store our token data in secure storage
+ * https://freek.dev/2103-replacing-keytar-with-electrons-safestorage-in-ray
+ *
+ * Data is saved at app.getPath('userData'):
+ * - Linux: ~/.config/finaldesktopapp/tokens.json
  */
 export class TokenStorage {
 
-    private static _key = 'DesktopSample.Tokens';
+    private static _key = 'EncryptedData';
     private static _store = new Store<Record<string, string>>({
-        name: 'desktopsample_tokens'
+        name: 'tokens'
     });
 
     /*
      * Load token data or return null
      */
     public static load(): TokenData | null {
+
+        console.log('*** ENCRYPTION ***');
+        console.log(safeStorage.isEncryptionAvailable());
 
         const encryptedBytesBase64 = this._store.get(TokenStorage._key);
         if (!encryptedBytesBase64) {
@@ -30,6 +37,10 @@ export class TokenStorage {
      * Save token data after login
      */
     public static save(data: TokenData): void {
+
+        if (!safeStorage.isEncryptionAvailable()) {
+            throw new Error('Cannot store tokens since encryption is unavailable');
+        }
 
         const json = JSON.stringify(data);
         const buffer = safeStorage.encryptString(json);
