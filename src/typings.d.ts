c0fd1a910aafd5b99321ce6d63b30cf8f72ff224
gary <gary.archer36@gmail.com>
2024-02-15 20:35:26 +0000

    Updates

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,7 +2,6 @@
 declare module 'electron-default-menu';
 declare module 'electron-packager';
 declare module 'opener';
-declare module 'username';
 
 // This is set by webpack and we use it to show stack traces in development builds
 declare const SHOW_STACK_TRACE: boolean;
