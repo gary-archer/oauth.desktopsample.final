7f9fcf1eb87f510079ba4f39d27c88e5af588dc3
gary-archer <garyarcher36@gmail.com>
2024-09-22 15:13:08 +0100

    Updates

--- src/renderer/fetchCacheKeys.ts
+++
@@ -1,9 +0,0 @@
-/*
- * String constants used as cache keys
- */
-export class FetchCacheKeys {
-    public static readonly Companies = 'companies';
-    public static readonly Transactions = 'transactions';
-    public static readonly OAuthUserInfo = 'oauthuserinfo';
-    public static readonly ApiUserInfo = 'apiuserinfo';
-}
